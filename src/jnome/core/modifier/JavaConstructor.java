02c8aca7aa79386532f740aaefb32df7fdea9ec1
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-07-22 16:19:24 +0200

    initial import

---
+++ src/jnome/core/modifier/JavaConstructor.java
@@ -0,0 +1,31 @@
+package jnome.core.modifier;
+
+import org.rejuse.property.PropertySet;
+
+import chameleon.core.element.Element;
+import chameleon.support.modifier.Constructor;
+/**
+ * A class for Java constructor modifiers.
+ * 
+ * Difference with super class:
+ *   1) A Java constructor is not inheritable. 
+ *  
+ * @author Marko
+ */
+public class JavaConstructor extends Constructor {
+
+	/**
+	 * A Java constructor is not inheritable.
+	 */
+ /*@
+   @ also public behavior
+   @
+   @ post \result.contains(language().CONSTRUCTOR);
+   @*/
+  public PropertySet<Element> impliedProperties() {
+  	PropertySet<Element> result = super.impliedProperties();
+    result.add(language().INHERITABLE.inverse());
+    return result;
+  }
+
+}

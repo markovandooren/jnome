3cfa3566204e44380c37938a189b10757358871f
marko <marko@192.168.0.2>
2009-07-29 00:14:01 +0200

    use junit4 now

--- src/jnome/test/AllTests.java
+++ src/jnome/test/AllTests.java
@@ -1,19 +1,18 @@
 package jnome.test;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-
-/**
- * @author Marko van Dooren
- */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestRejuse.class,TestJnome.class,TestAntlr.class,TestJnomeClone.class,TestGenerics.class})
 public class AllTests {
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for new jnome structure");
-//	  suite.addTestSuite(TestRejuse.class);
-//    suite.addTestSuite(TestJnome.class);
-//    suite.addTestSuite(TestAntlr.class);
-		return suite;
-	}
+
+//	public static Test suite() {
+//		TestSuite suite = new TestSuite("Test for new jnome structure");
+////	  suite.addTestSuite(TestRejuse.class);
+////    suite.addTestSuite(TestJnome.class);
+////    suite.addTestSuite(TestAntlr.class);
+//		return suite;
+//	}
 }

63971f52b17ea7b65bac9f310cafe7879503df12
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-09-11 17:12:20 +0200

    cleaned up imports

--- src/jnome/test/JavaTest.java
+++ src/jnome/test/JavaTest.java
@@ -2,9 +2,11 @@ package jnome.test;
 
 import org.junit.Test;
 
+import chameleon.core.reference.CrossReference;
 import chameleon.core.type.Type;
 import chameleon.support.test.ExpressionTest;
 import chameleon.test.CloneAndChildTest;
+import chameleon.test.CrossReferenceTest;
 import chameleon.test.provider.BasicDescendantProvider;
 import chameleon.test.provider.BasicNamespaceProvider;
 import chameleon.test.provider.ElementProvider;
@@ -12,18 +14,41 @@ import chameleon.test.provider.ModelProvider;
 
 public abstract class JavaTest {
 
+	/**
+	 * Test getType expressions in the namespaces provided
+	 * by the namespace provider.
+	 */
 	@Test
 	public void testExpressions() throws Exception {
 		new ExpressionTest(modelProvider(), typeProvider()).testExpressionTypes();
 	}
 
+	/**
+	 * Test clone and children for all elements in the namespaces provided
+	 * by the namespace provider.
+	 */
 	@Test
 	public void testCloneAndChildren() throws Exception {
 		new CloneAndChildTest(modelProvider(), namespaceProvider()).testClone();
 	}
+	
+	/**
+	 * Test getElement and getDeclarator for all cross-reference in all namespaces
+	 * provided by the namespace provider.
+	 */
+	@Test
+	public void testCrossReferences() throws Exception {
+		new CrossReferenceTest(modelProvider(), new BasicDescendantProvider<CrossReference>(namespaceProvider(), CrossReference.class));
+	}
 
+	/**
+	 * A provider for the model to be tested.
+	 */
 	public abstract ModelProvider modelProvider();
 
+	/**
+	 * A provider for the namespaces to be tested.
+	 */
 	public abstract BasicNamespaceProvider namespaceProvider();
 	
 	public ElementProvider<Type> typeProvider() {
--- src/jnome/test/TestAntlr.java
+++ src/jnome/test/TestAntlr.java
@@ -1,21 +1,8 @@
 package jnome.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jnome.input.JavaModelFactory;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.junit.Test;
-
-import chameleon.core.lookup.LookupException;
-import chameleon.core.type.Type;
-import chameleon.support.test.ExpressionTest;
-import chameleon.test.provider.BasicDescendantProvider;
 import chameleon.test.provider.BasicModelProvider;
 import chameleon.test.provider.BasicNamespaceProvider;
-import chameleon.test.provider.ElementProvider;
 import chameleon.test.provider.ModelProvider;
 
 /**

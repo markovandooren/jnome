4f189393830de804a310dd785a2fc81a85aae4ee
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-10-08 17:29:08 +0200

    extracted composite test

--- src/jnome/test/JavaTest.java
+++ src/jnome/test/JavaTest.java
@@ -3,26 +3,14 @@ package jnome.test;
 import org.junit.Before;
 import org.junit.Test;
 
-import chameleon.core.Config;
-import chameleon.core.namespacepart.NamespacePart;
-import chameleon.core.reference.CrossReference;
 import chameleon.core.type.Type;
 import chameleon.support.test.ExpressionTest;
-import chameleon.test.CloneAndChildTest;
-import chameleon.test.CrossReferenceTest;
-import chameleon.test.VerificationTest;
+import chameleon.test.CompositeTest;
 import chameleon.test.provider.BasicDescendantProvider;
-import chameleon.test.provider.BasicNamespaceProvider;
 import chameleon.test.provider.ElementProvider;
-import chameleon.test.provider.ModelProvider;
 
-public abstract class JavaTest {
+public abstract class JavaTest extends CompositeTest {
 	
-	@Before
-	public void setCaching() {
-    Config.setCaching(true);
-	}
-
 	/**
 	 * Set the log levels of Log4j. By default, nothing is changed.
 	 */
@@ -40,42 +28,6 @@ public abstract class JavaTest {
 		new ExpressionTest(modelProvider(), typeProvider()).testExpressionTypes();
 	}
 
-	/**
-	 * Test clone and children for all elements in the namespaces provided
-	 * by the namespace provider.
-	 */
-	@Test
-	public void testCloneAndChildren() throws Exception {
-		new CloneAndChildTest(modelProvider(), namespaceProvider()).testClone();
-	}
-	
-	/**
-	 * Test getElement and getDeclarator for all cross-reference in all namespaces
-	 * provided by the namespace provider.
-	 */
-	@Test
-	public void testCrossReferences() throws Exception {
-		new CrossReferenceTest(modelProvider(), new BasicDescendantProvider<CrossReference>(namespaceProvider(), CrossReference.class)).testCrossReferences();
-	}
-	
-	/**
-	 * Test the verification by invoking verify() for all namespace parts, and checking if the result is valid.
-	 */
-	@Test
-	public void testVerification() throws Exception {
-		new VerificationTest(modelProvider(), new BasicDescendantProvider<NamespacePart>(namespaceProvider(), NamespacePart.class)).testVerification();
-	}
-
-	/**
-	 * A provider for the model to be tested.
-	 */
-	public abstract ModelProvider modelProvider();
-
-	/**
-	 * A provider for the namespaces to be tested.
-	 */
-	public abstract BasicNamespaceProvider namespaceProvider();
-	
 	public ElementProvider<Type> typeProvider() {
 		return new BasicDescendantProvider<Type>(namespaceProvider(), Type.class);
 	}

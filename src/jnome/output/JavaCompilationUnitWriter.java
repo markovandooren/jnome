c32bc3b8fc5395ec44198904d63c2ff8b55180f8
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2011-10-13 00:40:16 +0200

    backup

---
+++ src/jnome/output/JavaCompilationUnitWriter.java
@@ -0,0 +1,43 @@
+package jnome.output;
+
+import java.io.File;
+
+import chameleon.core.compilationunit.CompilationUnit;
+import chameleon.core.lookup.LookupException;
+import chameleon.exception.ModelException;
+import chameleon.oo.type.Type;
+import chameleon.plugin.Plugin;
+import chameleon.plugin.build.CompilationUnitWriter;
+
+public class JavaCompilationUnitWriter extends CompilationUnitWriter {
+
+
+	public JavaCompilationUnitWriter(File outputDir, String extension) {
+		super(outputDir,extension);
+	}
+
+	public String fileName(CompilationUnit compilationUnit) throws LookupException, ModelException {
+		Type result = mainType(compilationUnit);
+		String name = (result == null ? null : result.getName()+extension());
+		return name;
+	}
+
+	public String packageFQN(CompilationUnit compilationUnit) throws LookupException, ModelException {
+		return mainType(compilationUnit).getNamespace().getFullyQualifiedName();
+	}
+	
+	private Type mainType(CompilationUnit compilationUnit) throws LookupException, ModelException {
+		Type result = null;
+		for(Type type: compilationUnit.descendants(Type.class)) {
+			if((type.nearestAncestor(Type.class) == null) && ((result == null) || (type.scope().ge(result.scope())))) {
+				result = type;
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public Plugin clone() {
+		return new JavaCompilationUnitWriter(outputDir(), extension());
+	}
+}

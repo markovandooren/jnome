c739595e047ce3c0f6490a49d82ee342f936ccc3
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-08-11 12:54:04 +0200

    added missing files

---
+++ src/jnome/core/language/JavaLanguageFactory.java
@@ -0,0 +1,19 @@
+package jnome.core.language;
+
+import jnome.input.JavaFactory;
+import jnome.input.JavaModelFactory;
+import jnome.output.JavaCodeWriter;
+import chameleon.input.ModelFactory;
+import chameleon.oo.plugin.ObjectOrientedFactory;
+import chameleon.plugin.output.Syntax;
+
+public class JavaLanguageFactory {
+
+	public Java create() {
+		Java result = new Java();
+		result.setPlugin(ModelFactory.class, new JavaModelFactory());
+		result.setPlugin(Syntax.class, new JavaCodeWriter());
+		result.setPlugin(ObjectOrientedFactory.class, new JavaFactory());
+		return result;
+	}
+}

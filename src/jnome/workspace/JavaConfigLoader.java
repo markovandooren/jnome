2da5e03ff68e1bece4b0d12a0bf07f292a74f455
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-10-09 12:27:19 +0200

    added missing files

---
+++ src/jnome/workspace/JavaConfigLoader.java
@@ -0,0 +1,34 @@
+package jnome.workspace;
+
+import java.io.File;
+
+import jnome.input.LazyJavaFileInputSourceFactory;
+import chameleon.core.language.Language;
+import chameleon.core.namespace.LazyRootNamespace;
+import chameleon.plugin.Plugin;
+import chameleon.plugin.PluginImpl;
+import chameleon.workspace.ConfigElement;
+import chameleon.workspace.ConfigException;
+import chameleon.workspace.ConfigLoader;
+import chameleon.workspace.View;
+
+public class JavaConfigLoader extends PluginImpl implements ConfigLoader {
+
+	public JavaConfigLoader(String javaBaseJarPath) {
+		_basePath = javaBaseJarPath;
+	}
+	
+	private String _basePath;
+	
+	@Override
+	public ConfigElement createConfigElement(Language language, String projectName, File root) throws ConfigException {
+		View view = new View(new LazyRootNamespace(), language);
+		return new JavaProjectConfig(view, new LazyJavaFileInputSourceFactory(), projectName, root, _basePath);
+	}
+
+	@Override
+	public Plugin clone() {
+		return new JavaConfigLoader(_basePath);
+	}
+
+}

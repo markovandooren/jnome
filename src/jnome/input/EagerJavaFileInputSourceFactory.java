6be16b4abd3c80aab08a4b57d9dd5393924c4494
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-01-16 23:26:22 +0100

    improvements in input sources

--- src/jnome/input/EagerJavaFileInputSourceFactory.java
+++ src/jnome/input/EagerJavaFileInputSourceFactory.java
@@ -3,17 +3,19 @@ package jnome.input;
 import java.io.File;
 
 import chameleon.core.namespace.InputSourceNamespace;
+import chameleon.util.Util;
 import chameleon.workspace.DirectoryLoader;
 import chameleon.workspace.FileInputSourceFactory;
+import chameleon.workspace.IFileInputSource;
 import chameleon.workspace.InputException;
-import chameleon.workspace.InputSource;
 import chameleon.workspace.LazyFileInputSource;
 
 public class EagerJavaFileInputSourceFactory extends FileInputSourceFactory {
 
 	@Override
-	public InputSource create(File file, DirectoryLoader loader) throws InputException {
-		LazyFileInputSource eagerJavaFileInputSource = new LazyFileInputSource(file,(InputSourceNamespace) currentNamespace());
+	public IFileInputSource create(File file, DirectoryLoader loader) throws InputException {
+		String declarationName = Util.getAllButLastPart(file.getName());
+		LazyFileInputSource eagerJavaFileInputSource = new LazyFileInputSource(file,declarationName,(InputSourceNamespace) currentNamespace());
 		loader.addInputSource(eagerJavaFileInputSource);
 		eagerJavaFileInputSource.load();
 		return eagerJavaFileInputSource;
--- src/jnome/input/LazyJavaFileInputSourceFactory.java
+++ src/jnome/input/LazyJavaFileInputSourceFactory.java
@@ -3,17 +3,19 @@ package jnome.input;
 import java.io.File;
 
 import chameleon.core.namespace.InputSourceNamespace;
+import chameleon.util.Util;
 import chameleon.workspace.DirectoryLoader;
 import chameleon.workspace.FileInputSourceFactory;
+import chameleon.workspace.IFileInputSource;
 import chameleon.workspace.InputException;
-import chameleon.workspace.InputSource;
 import chameleon.workspace.LazyFileInputSource;
 
 public class LazyJavaFileInputSourceFactory extends FileInputSourceFactory {
 
 	@Override
-	public InputSource create(File file, DirectoryLoader loader) throws InputException {
-		LazyFileInputSource javaFileInputSource = new LazyFileInputSource(file, (InputSourceNamespace) currentNamespace());
+	public IFileInputSource create(File file, DirectoryLoader loader) throws InputException {
+		String declarationName = Util.getAllButLastPart(file.getName());
+		LazyFileInputSource javaFileInputSource = new LazyFileInputSource(file, declarationName, (InputSourceNamespace) currentNamespace());
 		loader.addInputSource(javaFileInputSource);
 		return javaFileInputSource;
 	}

508f973fb6ff0ccce4df5d086a1536bd971177f3
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-06-14 19:22:48 +0200

    more finetuning of the dependency analyser

--- src/be/kuleuven/cs/distrinet/jnome/tool/dependency/NoSubtypeOf.java
+++ src/be/kuleuven/cs/distrinet/jnome/tool/dependency/NoSubtypeOf.java
@@ -1,13 +1,11 @@
 package be.kuleuven.cs.distrinet.jnome.tool.dependency;
 
-import java.util.Set;
-
 import be.kuleuven.cs.distrinet.chameleon.core.lookup.LookupException;
 import be.kuleuven.cs.distrinet.chameleon.oo.type.Type;
-import be.kuleuven.cs.distrinet.chameleon.util.Pair;
+import be.kuleuven.cs.distrinet.chameleon.util.Util;
 import be.kuleuven.cs.distrinet.rejuse.predicate.SafePredicate;
 
-public class NoSubtypeOf extends SafePredicate<Pair<Type,Set<Type>>> {
+public class NoSubtypeOf extends SafePredicate<Type> {
 
 	public NoSubtypeOf(Type superType) {
 		_superType = superType;
@@ -16,9 +14,10 @@ public class NoSubtypeOf extends SafePredicate<Pair<Type,Set<Type>>> {
 	private Type _superType;
 	
 	@Override
-	public boolean eval(Pair<Type,Set<Type>> object) {
+	public boolean eval(Type object) {
 		try {
-			return ! object.first().subTypeOf(_superType);
+			boolean subTypeOf = object.subTypeOf(_superType);
+			return ! subTypeOf;
 		} catch (LookupException e) {
 			return false;
 		}

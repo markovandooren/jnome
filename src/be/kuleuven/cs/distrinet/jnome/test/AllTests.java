d6770cfeab59b52d09771ddb7a5e21489da9467f
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-09-09 21:26:08 +0200

    added weka test

--- src/be/kuleuven/cs/distrinet/jnome/test/AllTests.java
+++ src/be/kuleuven/cs/distrinet/jnome/test/AllTests.java
@@ -5,13 +5,14 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	TestRejuse.class,
-	TestJnome.class,
-	TestAntlr.class,
-	TestCustomCases.class,
-	TestGenericRejuse.class,
-	TestChameleon.class,
-	TestChameleonSupport.class,
-	TestProjectConfig.class
+	TestRejuse.class
+	,TestJnome.class
+	,TestAntlr.class
+	,TestCustomCases.class
+	,TestGenericRejuse.class
+	,TestChameleon.class
+	,TestChameleonSupport.class
+	,TestProjectConfig.class
+//	,TestWeka.class
 	})
 public class AllTests {}
---
+++ src/be/kuleuven/cs/distrinet/jnome/test/TestWeka.java
@@ -0,0 +1,45 @@
+package be.kuleuven.cs.distrinet.jnome.test;
+
+import java.io.File;
+import java.util.Collection;
+
+import be.kuleuven.cs.distrinet.chameleon.oo.type.Type;
+import be.kuleuven.cs.distrinet.chameleon.test.provider.BasicDescendantProvider;
+import be.kuleuven.cs.distrinet.chameleon.test.provider.BasicNamespaceProvider;
+import be.kuleuven.cs.distrinet.chameleon.test.provider.ElementProvider;
+import be.kuleuven.cs.distrinet.chameleon.workspace.View;
+import be.kuleuven.cs.distrinet.rejuse.predicate.SafePredicate;
+
+/**
+ * @author Marko van Dooren
+ */
+public class TestWeka extends JavaTest {
+
+	@Override
+	protected File projectFile() {
+		return new File(TEST_DATA,"testweka.xml");
+	}
+
+	@Override
+	public BasicNamespaceProvider namespaceProvider() {
+		return new BasicNamespaceProvider("weka");
+	}
+
+	public ElementProvider<Type> typeProvider() {
+		return new ElementProvider<Type>() {
+
+			public Collection<Type> elements(View view) {
+				Collection<Type> types = new BasicDescendantProvider<Type>(namespaceProvider(), Type.class).elements(view);
+				new SafePredicate<Type>() {
+
+					@Override
+					public boolean eval(Type object) {
+						return object.getFullyQualifiedName().equals("weka.attributeSelection.PrincipalComponents");
+					}
+				}.filter(types);
+				return types;
+			}
+		};
+	}
+
+}

2a3befc7ddb721739c7816d56e6b2273e92fabd3
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-03-26 00:21:26 +0100

    use direct type reference for default inheritance relation

--- src/be/kuleuven/cs/distrinet/jnome/core/type/RegularJavaType.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/RegularJavaType.java
@@ -44,131 +44,131 @@ import com.google.common.collect.ImmutableList.Builder;
 
 public class RegularJavaType extends RegularType implements JavaType {
 
-	public RegularJavaType(String name) {
-		super(name);
-		setDefaultDefaultConstructor(false);
-	}
-	
-//	private StackOverflowTracer _tracer = new StackOverflowTracer(3);
-	
-	public DerivedType createDerivedType(List<ActualTypeArgument> typeArguments) throws LookupException {
-//		if(typeArguments.size() == 1) {
-//			if(_genericCache == null) {
-//				synchronized(this) {
-//					if(_genericCache == null) {
-//						_genericCache = new HashMap<>();
-//					}
-//				}
-//			}
-//			_tracer.push();
-//			Type key = typeArguments.get(0).type();
-//			_tracer.pop();
-//			DerivedType result = _genericCache.get(key);
-//			if(result == null) {
-//				synchronized(this) {
-//					if(result == null) {
-//						result = new JavaDerivedType(this,typeArguments);
-//						_genericCache.put(key, result);
-//					}
-//				}
-//			} else {
-////				System.out.println(++COUNT+" generic cache hit for "+name()+"<"+key.name()+">");
-//			}
-//			return result;
-//		}
+  public RegularJavaType(String name) {
+    super(name);
+    setDefaultDefaultConstructor(false);
+  }
 
-//		int size = typeArguments.size();
-//		Integer current = COUNTMAP.get(size);
-//		if(current == null) {
-//			current = new Integer(0);
-//		}
-//		Integer value = new Integer(current.intValue() + 1);
-//		System.out.println("Size: "+size+" count: "+value);
-//		COUNTMAP.put(size,value);
-		return new JavaDerivedType(this,typeArguments);
-	}
-	
-	
-	
-//	private static int COUNT;
-//	private static Map<Integer,Integer> COUNTMAP = new HashMap<>(); 
-	
-	
-//	private Map<Type,DerivedType> _genericCache;
+  //	private StackOverflowTracer _tracer = new StackOverflowTracer(3);
 
-	protected RegularType cloneSelf() {
-		RegularJavaType regularJavaType = new RegularJavaType(name());
-		regularJavaType.parameterBlock(TypeParameter.class).disconnect();
-		return regularJavaType;
-	}
+  public DerivedType createDerivedType(List<ActualTypeArgument> typeArguments) throws LookupException {
+    //		if(typeArguments.size() == 1) {
+    //			if(_genericCache == null) {
+    //				synchronized(this) {
+    //					if(_genericCache == null) {
+    //						_genericCache = new HashMap<>();
+    //					}
+    //				}
+    //			}
+    //			_tracer.push();
+    //			Type key = typeArguments.get(0).type();
+    //			_tracer.pop();
+    //			DerivedType result = _genericCache.get(key);
+    //			if(result == null) {
+    //				synchronized(this) {
+    //					if(result == null) {
+    //						result = new JavaDerivedType(this,typeArguments);
+    //						_genericCache.put(key, result);
+    //					}
+    //				}
+    //			} else {
+    ////				System.out.println(++COUNT+" generic cache hit for "+name()+"<"+key.name()+">");
+    //			}
+    //			return result;
+    //		}
 
-	protected NormalMethod defaultDefaultConstructor() {
-		return _defaultDefaultConstructor;
-	}
-	
-	private NormalMethod _defaultDefaultConstructor;
-	
-	/**
-	 * Set the default default constructor.
-	 */
-	protected void setDefaultDefaultConstructor(boolean rebuildCache) {
-		JavaMethod cons = createDefaultConstructorWithoutAccessModifier(rebuildCache);
-		cons.addModifier(new Public());
-	}
+    //		int size = typeArguments.size();
+    //		Integer current = COUNTMAP.get(size);
+    //		if(current == null) {
+    //			current = new Integer(0);
+    //		}
+    //		Integer value = new Integer(current.intValue() + 1);
+    //		System.out.println("Size: "+size+" count: "+value);
+    //		COUNTMAP.put(size,value);
+    return new JavaDerivedType(this,typeArguments);
+  }
 
-	/**
-	 * Create a default default constructor without an access modifier.
-	 */
-	protected JavaMethod createDefaultConstructorWithoutAccessModifier(boolean rebuildCache) {
-		// FIXME Because this code is ran when a regular Java type is constructed, we cannot ask the
-		//       language for the factory. Management of the constructor should be done lazily. When
-		//       the type is actually used, we can assume that a language is attached. Otherwise, we
-		//       throw an exception.
-		JavaMethod cons = new JavaMethod(new SimpleNameMethodHeader(name(), new BasicJavaTypeReference(name())));
-		cons.addModifier(new Constructor());
-		Block body = new Block();
-		cons.setImplementation(new RegularImplementation(body));
-		body.addStatement(new StatementExpression(new SuperConstructorDelegation()));
-		cons.setUniParent(this);
-		setDefaultDefaultConstructor(cons);
-		return cons;
-	}
-	
-	protected void clearDefaultDefaultConstructor() {
-		setDefaultDefaultConstructor(null);
-	}
-	
-	private void setDefaultDefaultConstructor(JavaMethod method) {
-		_defaultDefaultConstructor = method;
-		_implicitMemberCache = null;
-	}
-	
-	/**
-	 * A Java reference type has a default constructor when
-	 * no other constructor is present.
-	 */
+
+
+  //	private static int COUNT;
+  //	private static Map<Integer,Integer> COUNTMAP = new HashMap<>(); 
+
+
+  //	private Map<Type,DerivedType> _genericCache;
+
+  protected RegularType cloneSelf() {
+    RegularJavaType regularJavaType = new RegularJavaType(name());
+    regularJavaType.parameterBlock(TypeParameter.class).disconnect();
+    return regularJavaType;
+  }
+
+  protected NormalMethod defaultDefaultConstructor() {
+    return _defaultDefaultConstructor;
+  }
+
+  private NormalMethod _defaultDefaultConstructor;
+
+  /**
+   * Set the default default constructor.
+   */
+  protected void setDefaultDefaultConstructor(boolean rebuildCache) {
+    JavaMethod cons = createDefaultConstructorWithoutAccessModifier(rebuildCache);
+    cons.addModifier(new Public());
+  }
+
+  /**
+   * Create a default default constructor without an access modifier.
+   */
+  protected JavaMethod createDefaultConstructorWithoutAccessModifier(boolean rebuildCache) {
+    // FIXME Because this code is ran when a regular Java type is constructed, we cannot ask the
+    //       language for the factory. Management of the constructor should be done lazily. When
+    //       the type is actually used, we can assume that a language is attached. Otherwise, we
+    //       throw an exception.
+    JavaMethod cons = new JavaMethod(new SimpleNameMethodHeader(name(), new BasicJavaTypeReference(name())));
+    cons.addModifier(new Constructor());
+    Block body = new Block();
+    cons.setImplementation(new RegularImplementation(body));
+    body.addStatement(new StatementExpression(new SuperConstructorDelegation()));
+    cons.setUniParent(this);
+    setDefaultDefaultConstructor(cons);
+    return cons;
+  }
+
+  protected void clearDefaultDefaultConstructor() {
+    setDefaultDefaultConstructor(null);
+  }
+
+  private void setDefaultDefaultConstructor(JavaMethod method) {
+    _defaultDefaultConstructor = method;
+    _implicitMemberCache = null;
+  }
+
+  /**
+   * A Java reference type has a default constructor when
+   * no other constructor is present.
+   */
   @Override
-	public List<Member> implicitMembers() {
-  	if(_implicitMemberCache == null) {
-  		_implicitMemberCache = buildImplicitMembersCache();
-  	}
-  	return _implicitMemberCache;
-	}
+  public List<Member> implicitMembers() {
+    if(_implicitMemberCache == null) {
+      _implicitMemberCache = buildImplicitMembersCache();
+    }
+    return _implicitMemberCache;
+  }
 
-	protected List<Member> buildImplicitMembersCache() {
-  	Builder<Member> builder = ImmutableList.<Member>builder();
-  	NormalMethod defaultDefaultConstructor = defaultDefaultConstructor();
-  	if(defaultDefaultConstructor != null) {
-  		builder.add(defaultDefaultConstructor);
-  	}
-  	NormalMethod classMethod = getClassMethod();
-  	if(classMethod != null) {
-  		builder.add(classMethod);
-  	}
-  	return builder.build();
-	}
-	
-	private List<Member> _implicitMemberCache;
+  protected List<Member> buildImplicitMembersCache() {
+    Builder<Member> builder = ImmutableList.<Member>builder();
+    NormalMethod defaultDefaultConstructor = defaultDefaultConstructor();
+    if(defaultDefaultConstructor != null) {
+      builder.add(defaultDefaultConstructor);
+    }
+    NormalMethod classMethod = getClassMethod();
+    if(classMethod != null) {
+      builder.add(classMethod);
+    }
+    return builder.build();
+  }
+
+  private List<Member> _implicitMemberCache;
 
   /**
    * This is actually cheating because the getClass method in Java
@@ -178,22 +178,22 @@ public class RegularJavaType extends RegularType implements JavaType {
    * @return
    */
   public NormalMethod getClassMethod() {
-  	if(_getClassMethod == null) {
-  		if(view(JavaView.class).topLevelType() != this) {
-  			Java7 language = language(Java7.class);
-  			BasicJavaTypeReference returnType = language.createTypeReference("java.lang.Class");
-  			JavaTypeReference erasedThisType = language.createTypeReference(name());
-  			ActualTypeArgument arg = language.createExtendsWildcard(erasedThisType);
-  			returnType.addArgument(arg);
-  			_getClassMethod = new NormalMethod(new SimpleNameMethodHeader("getClass", returnType));
-  			_getClassMethod.addModifier(new Public());
-  			_getClassMethod.addModifier(new Native());
-  			_getClassMethod.setUniParent(body());
-  		}
-  	}
-  	return _getClassMethod;
+    if(_getClassMethod == null) {
+      if(view(JavaView.class).topLevelType() != this) {
+        Java7 language = language(Java7.class);
+        BasicJavaTypeReference returnType = language.createTypeReference("java.lang.Class");
+        JavaTypeReference erasedThisType = language.createTypeReference(name());
+        ActualTypeArgument arg = language.createExtendsWildcard(erasedThisType);
+        returnType.addArgument(arg);
+        _getClassMethod = new NormalMethod(new SimpleNameMethodHeader("getClass", returnType));
+        _getClassMethod.addModifier(new Public());
+        _getClassMethod.addModifier(new Native());
+        _getClassMethod.setUniParent(body());
+      }
+    }
+    return _getClassMethod;
   }
-  
+
   private NormalMethod _getClassMethod;
 
   /**
@@ -201,13 +201,13 @@ public class RegularJavaType extends RegularType implements JavaType {
    * constructor is removed.
    */
   public void reactOnDescendantAdded(Element element) {
-  	if(element instanceof TypeElement) {
-  		if(isConstructor(element)) {
-		    clearDefaultDefaultConstructor();
-  		}
-  	}
+    if(element instanceof TypeElement) {
+      if(isConstructor(element)) {
+        clearDefaultDefaultConstructor();
+      }
+    }
   }
-  
+
   /**
    * A Java reference type is not overridable. A such, if B extends A,
    * and both A and B have a nested class with name C, then B.C does
@@ -215,133 +215,133 @@ public class RegularJavaType extends RegularType implements JavaType {
    */
   @Override
   public PropertySet<Element, ChameleonProperty> inherentProperties() {
-  	PropertySet<Element, ChameleonProperty> result = new PropertySet<Element, ChameleonProperty>();
-  	result.add(language(ObjectOrientedLanguage.class).OVERRIDABLE.inverse());
-  	return result;
+    PropertySet<Element, ChameleonProperty> result = new PropertySet<Element, ChameleonProperty>();
+    result.add(language(ObjectOrientedLanguage.class).OVERRIDABLE.inverse());
+    return result;
   }
 
-	private boolean isConstructor(Element element) {
-		//FIXME element.isTrue(language(Java.class).CONSTRUCTOR) doesn't work since the type
-		//      and the constructor aren't connected to the model during parsing.
-		// The suck is strong in this one
-		List<Modifier> mods = ((TypeElement) element).modifiers();
-		for(Modifier mod:mods) {
-			if(mod instanceof JavaConstructor) {
-				return true;
-			}
-		}
-		return false;
-	}
+  private boolean isConstructor(Element element) {
+    //FIXME element.isTrue(language(Java.class).CONSTRUCTOR) doesn't work since the type
+    //      and the constructor aren't connected to the model during parsing.
+    // The suck is strong in this one
+    List<Modifier> mods = ((TypeElement) element).modifiers();
+    for(Modifier mod:mods) {
+      if(mod instanceof JavaConstructor) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-	/**
-	 * If an element is removed, we check whether it is the
-	 * last remaining constructor. If it is, we add the
-	 * default default constructor.
-	 */
+  /**
+   * If an element is removed, we check whether it is the
+   * last remaining constructor. If it is, we add the
+   * default default constructor.
+   */
   public void reactOnDescendantRemoved(Element element) {
-  	if(isConstructor(element)) {
-  		List<TypeElement> elements = body().elements();
-  		for(TypeElement el: elements) {
-  			if(isConstructor(el)) {
-  				return;
-  			}
-  		}
-  		setDefaultDefaultConstructor(true);
-  	}
+    if(isConstructor(element)) {
+      List<TypeElement> elements = body().elements();
+      for(TypeElement el: elements) {
+        if(isConstructor(el)) {
+          return;
+        }
+      }
+      setDefaultDefaultConstructor(true);
+    }
   }
 
   public void reactOnDescendantReplaced(Element oldElement, Element newElement) {
-  	reactOnDescendantRemoved(oldElement);
-  	reactOnDescendantAdded(newElement);
+    reactOnDescendantRemoved(oldElement);
+    reactOnDescendantAdded(newElement);
   }
-  
+
   @Override
   public List<InheritanceRelation> implicitNonMemberInheritanceRelations() {
-  	//FIXME speed avoid creating collection
-    String defaultSuperClassFQN = language(ObjectOrientedLanguage.class).getDefaultSuperClassFQN();
-		if(explicitNonMemberInheritanceRelations().isEmpty() && 
-    	 (! getFullyQualifiedName().equals(defaultSuperClassFQN)) //"java.lang.Object"
-    	) {
-//    	InheritanceRelation relation = new SubtypeRelation(language(ObjectOrientedLanguage.class).createTypeReference(new NamedTarget("java.lang"),"Object"));
-    	InheritanceRelation relation = new SubtypeRelation(language(ObjectOrientedLanguage.class).createTypeReference(defaultSuperClassFQN));
-    	relation.setUniParent(this);
-    	relation.setMetadata(new TagImpl(), IMPLICIT_CHILD);
-    	List<InheritanceRelation> result = new ArrayList<InheritanceRelation>(1);
-    	result.add(relation);
-    	return result;
-    } else {
-    	return Collections.EMPTY_LIST;
+    //FIXME speed avoid creating collection
+    if(explicitNonMemberInheritanceRelations().isEmpty()) {
+      JavaView view = view(JavaView.class);
+      if( 
+          view.topLevelType() != this) 
+      {
+        InheritanceRelation relation = new SubtypeRelation(new DirectJavaTypeReference(view(JavaView.class).topLevelType()));
+        relation.setUniParent(this);
+        relation.setMetadata(new TagImpl(), IMPLICIT_CHILD);
+        List<InheritanceRelation> result = new ArrayList<InheritanceRelation>(1);
+        result.add(relation);
+        return result;
+      }
     }
+    return Collections.EMPTY_LIST;
   }
-  
+
   @Override
   public boolean hasInheritanceRelation(InheritanceRelation relation) throws LookupException {
-  	return super.hasInheritanceRelation(relation) || relation.hasMetadata(IMPLICIT_CHILD);
+    return super.hasInheritanceRelation(relation) || relation.hasMetadata(IMPLICIT_CHILD);
   }
-  
+
   public final static String IMPLICIT_CHILD = "IMPLICIT CHILD";
 
-	@Override
-	public Type erasure() {
-		//FIXME this code seems to have been duplicated a number of times.
-		Java7 language = language(Java7.class);
-		RawType result = _rawTypeCache;
-		if(result == null) {
-				if(is(language.INSTANCE) == Ternary.TRUE) {
-					Type outmostType = farthestAncestor(Type.class);
-					if(outmostType == null) {
-						outmostType = this;
-					}
-					RawType outer;
-					if(outmostType instanceof RawType) {
-						outer = (RawType) outmostType;
-					} else {
-						outer = new RawType(outmostType);
-					}
-					RawType current = outer;
-					List<Type> outerTypes = ancestors(Type.class);
-					outerTypes.add(0, this);
+  @Override
+  public Type erasure() {
+    //FIXME this code seems to have been duplicated a number of times.
+    Java7 language = language(Java7.class);
+    RawType result = _rawTypeCache;
+    if(result == null) {
+      if(is(language.INSTANCE) == Ternary.TRUE) {
+        Type outmostType = farthestAncestor(Type.class);
+        if(outmostType == null) {
+          outmostType = this;
+        }
+        RawType outer;
+        if(outmostType instanceof RawType) {
+          outer = (RawType) outmostType;
+        } else {
+          outer = new RawType(outmostType);
+        }
+        RawType current = outer;
+        List<Type> outerTypes = ancestors(Type.class);
+        outerTypes.add(0, this);
 
-					int size = outerTypes.size();
-					Factory expressionFactory = language.plugin(Factory.class);
-					for(int i = size - 2; i>=0;i--) {
-						NameReference<RawType> simpleRef = expressionFactory.createNameReference(outerTypes.get(i).name(), (Class)RawType.class);
-						simpleRef.setUniParent(current);
-						try {
-							current = simpleRef.getElement();
-						} catch (LookupException e) {
-							e.printStackTrace();
-							throw new ChameleonProgrammerException("An inner type of a newly created outer raw type cannot be found",e);
-						}
-					}
-					result = current;
-				} else {
-					// static
-					result = new RawType(this);
-				}
-				_rawTypeCache = result;
-			}
-		  return result;	
-		}
+        int size = outerTypes.size();
+        Factory expressionFactory = language.plugin(Factory.class);
+        for(int i = size - 2; i>=0;i--) {
+          NameReference<RawType> simpleRef = expressionFactory.createNameReference(outerTypes.get(i).name(), (Class)RawType.class);
+          simpleRef.setUniParent(current);
+          try {
+            current = simpleRef.getElement();
+          } catch (LookupException e) {
+            e.printStackTrace();
+            throw new ChameleonProgrammerException("An inner type of a newly created outer raw type cannot be found",e);
+          }
+        }
+        result = current;
+      } else {
+        // static
+        result = new RawType(this);
+      }
+      _rawTypeCache = result;
+    }
+    return result;	
+  }
 
-	private RawType _rawTypeCache;
-	
-	public ArrayType toArray() {
-		if(_arrayType == null) {
-			_arrayType = new ArrayType(this);
-		}
-		return _arrayType;
-	}
-	
-	private ArrayType _arrayType;
-	
-	@Override
-	public synchronized void flushLocalCache() {
-		super.flushLocalCache();
-		_rawTypeCache = null;
-		_arrayType = null;
-		// The implicit member cache is kept up to date via the
-		// reactTo... methods.
-	}
+  private RawType _rawTypeCache;
+
+  public ArrayType toArray() {
+    if(_arrayType == null) {
+      _arrayType = new ArrayType(this);
+    }
+    return _arrayType;
+  }
+
+  private ArrayType _arrayType;
+
+  @Override
+  public synchronized void flushLocalCache() {
+    super.flushLocalCache();
+    _rawTypeCache = null;
+    _arrayType = null;
+    // The implicit member cache is kept up to date via the
+    // reactTo... methods.
+  }
 
 }

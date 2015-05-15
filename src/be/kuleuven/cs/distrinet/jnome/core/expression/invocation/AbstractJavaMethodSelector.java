ddf382f3468311c76fe12560aee2ebe6bed5e439
Marko van Dooren <Marko.van.Dooren@gmail.com>
2015-05-15 17:07:53 +0200

    speed up checking of most specific method

--- src/be/kuleuven/cs/distrinet/jnome/core/expression/invocation/AbstractJavaMethodSelector.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/expression/invocation/AbstractJavaMethodSelector.java
@@ -2,6 +2,7 @@ package be.kuleuven.cs.distrinet.jnome.core.expression.invocation;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -32,6 +33,8 @@ import be.kuleuven.cs.distrinet.jnome.core.type.ArrayType;
 import be.kuleuven.cs.distrinet.jnome.core.type.NullType;
 import be.kuleuven.cs.distrinet.jnome.core.type.RawType;
 import be.kuleuven.cs.distrinet.jnome.core.variable.MultiFormalParameter;
+import be.kuleuven.cs.distrinet.rejuse.logic.relation.PartialOrder;
+import be.kuleuven.cs.distrinet.rejuse.logic.relation.Relation;
 import be.kuleuven.cs.distrinet.rejuse.logic.ternary.Ternary;
 
 public abstract class AbstractJavaMethodSelector<M extends Method> implements DeclarationSelector<M> {
@@ -46,6 +49,7 @@ public abstract class AbstractJavaMethodSelector<M extends Method> implements De
 	}
 
 	private Class<M> _type;
+//	private WeakPartialOrder<MethodSelectionResult> _order;
 
 	@Override
 	public boolean isGreedy() {
@@ -308,11 +312,30 @@ public abstract class AbstractJavaMethodSelector<M extends Method> implements De
 	}
 
 	protected void applyOrder(List<MethodSelectionResult> tmp) throws LookupException {
-		order().removeBiggerElements(tmp);
-	}
-
-	public WeakPartialOrder<MethodSelectionResult> order() {
-		return new JavaMostSpecificMethodOrder<MethodSelectionResult>(invocation());
+//		if(_order == null) {
+//			_order = new JavaMostSpecificMethodOrder<MethodSelectionResult>(invocation());
+//		}
+		int size = tmp.size();
+		int i=0;
+		outer: while(i< size) {
+			MethodSelectionResult e1 = tmp.get(i);
+			for(int j=i+1; j < size; j++) {
+				MethodSelectionResult e2 = tmp.get(j);
+				if(i != j) {
+					boolean firstSmaller = contains(e1,e2);
+					boolean secondSmaller = contains(e2,e1);
+					if(secondSmaller && (! firstSmaller)) {
+						tmp.remove(i);
+						size--;
+						continue outer;
+					} else if(firstSmaller && (! secondSmaller)) {
+						tmp.remove(j);
+						size--;
+					}
+				}
+			}
+			i++;
+		}
 	}
 
 	public Class<M> selectedClass() {
@@ -323,4 +346,133 @@ public abstract class AbstractJavaMethodSelector<M extends Method> implements De
 	public boolean canSelect(Class<? extends Declaration> type) {
 		return _type.isAssignableFrom(type);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public boolean contains(MethodSelectionResult firstResult, MethodSelectionResult secondResult) throws LookupException {
+		boolean result = false;
+		if(firstResult != secondResult) {
+			int firstPhase = firstResult.phase();
+			int secondPhase = secondResult.phase();
+			if(firstPhase < secondPhase) {
+				result = true;
+			} else if(firstPhase > secondPhase) {
+				result = false;
+			} else {
+				Method first = firstResult.template();
+				Method second = secondResult.template();
+				if(! first.sameAs(second)) {
+					if(!(first.lastFormalParameter() instanceof MultiFormalParameter) && ! (second.lastFormalParameter() instanceof MultiFormalParameter)) {
+						result = containsFixedArity(first, second);
+					} else if((first.lastFormalParameter() instanceof MultiFormalParameter) && (second.lastFormalParameter() instanceof MultiFormalParameter)){
+						result = containsVariableArity(firstResult, secondResult);
+					}
+					
+				}
+			}
+		}
+		return result;
+	}
+
+	public boolean containsVariableArity(MethodSelectionResult firstResult, MethodSelectionResult secondResult) throws LookupException {
+		Method first = firstResult.template();
+		Method second = secondResult.template();
+
+		boolean result = true;
+		Java7 language = (Java7) first.language(Java7.class);
+		List<Type> firstTypes = first.header().formalParameterTypes();
+		List<Type> secondTypes = second.header().formalParameterTypes();
+		int firstSize = firstTypes.size();
+		firstTypes.set(firstSize-1, ((ArrayType)firstTypes.get(firstSize-1)).elementType());
+		int secondSize = secondTypes.size();
+		secondTypes.set(secondSize-1, ((ArrayType)secondTypes.get(secondSize-1)).elementType());
+		int n;
+		int k;
+		if(firstSize >= secondSize) {
+			n = firstSize;
+			k = secondSize;
+		} else {
+			n = secondSize;
+			k = firstSize;
+		}
+		List typeParameters = second.typeParameters();
+		List<Type> Ss;
+		if(typeParameters.size() > 0) {
+			FirstPhaseConstraintSet constraints = new FirstPhaseConstraintSet(invocation(),second.header());
+			for(int i=0; i < k-1; i++) {
+				constraints.add(new SSConstraint(language.reference(firstTypes.get(i)), secondTypes.get(i)));
+			}
+			TypeAssignmentSet As = constraints.resolve();
+			result = As.valid();
+			if(result) {
+			  Ss = JavaMethodInvocation.formalParameterTypesInContext(second, As);
+			} else {
+				// If the actual type parameters are invalid, don't bother
+				// substituting them
+				Ss = null;
+			}
+		} else {
+			Ss = new ArrayList<Type>(secondTypes);
+		}
+		for(int i=0; result && i<k-1;i++) {
+			result = firstTypes.get(i).subTypeOf(Ss.get(i));
+		}
+		if(result && firstSize >= secondSize) {
+			for(int i=k-1; result && i<n;i++) {
+				result = firstTypes.get(i).subTypeOf(Ss.get(k-1));
+			}
+		} else {
+			for(int i=k-1; result && i<n;i++) {
+				result = firstTypes.get(k-1).subTypeOf(Ss.get(i));
+			}
+		}
+		return result;
+	}
+	
+	public boolean containsFixedArity(Method first, Method second) throws LookupException {
+		boolean result = true;
+		Java7 language = (Java7) first.language(Java7.class);
+		List<Type> Ts = first.header().formalParameterTypes();
+		List<Type> Us = second.header().formalParameterTypes();
+		int size =Ts.size();
+		boolean hasFormalTypeParameter = false;
+		for(TypeParameter p: second.typeParameters()) {
+			if(p instanceof FormalTypeParameter) {
+				hasFormalTypeParameter = true;
+				break;
+			}
+		}
+		List<Type> Ss;
+		if(hasFormalTypeParameter) {
+			FirstPhaseConstraintSet constraints = new FirstPhaseConstraintSet(invocation(), second.header());
+			for(int i=0; i < size; i++) {
+				constraints.add(new SSConstraint(language.reference(Ts.get(i)), Us.get(i)));
+			}
+			TypeAssignmentSet As = constraints.resolve();
+			result = As.valid();
+			if(result) {
+			  Ss = JavaMethodInvocation.formalParameterTypesInContext(second, As);
+			} else {
+				// If the actual type parameters are invalid, don't bother
+				// substituting them
+				Ss = null;
+			}
+		} else {
+			Ss = new ArrayList<Type>(Us);
+		}
+		for(int i=0; result && i<size;i++) {
+			result = Ts.get(i).subTypeOf(Ss.get(i));
+		}
+		
+		return result;
+	}
+
 }

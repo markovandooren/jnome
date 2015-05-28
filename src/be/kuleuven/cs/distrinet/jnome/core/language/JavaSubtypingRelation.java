7509fecc3ffa2b91a702180a032bfc56c509819d
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-28 17:28:34 +0200

    fix creation of type argument. insert non-local reference

--- src/be/kuleuven/cs/distrinet/jnome/core/language/JavaSubtypingRelation.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/language/JavaSubtypingRelation.java
@@ -436,7 +436,11 @@ public class JavaSubtypingRelation extends SubtypeRelation {
 		}
 
 		protected ActualTypeArgument createArgument(Type type) throws LookupException {
-			return java().createExtendsWildcard(java().reference(type));
+		  final JavaTypeReference reference = java().reference(type);
+		  Element parent = reference.parent();
+		  reference.setUniParent(null);
+		  final NonLocalJavaTypeReference nonLocal = new NonLocalJavaTypeReference(reference,parent);
+      return java().createExtendsWildcard(nonLocal);
 		}
 
 		private Binder _next;

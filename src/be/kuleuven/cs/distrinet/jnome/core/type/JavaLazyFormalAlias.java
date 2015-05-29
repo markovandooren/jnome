23475d699f1398c1cd935124fa72f8c2b7ecbdf5
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-29 09:50:17 +0200

    Update the type fixer to avoid an infinite loop for recursively defined captured type parameters

--- src/be/kuleuven/cs/distrinet/jnome/core/type/JavaLazyFormalAlias.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/JavaLazyFormalAlias.java
@@ -1,6 +1,8 @@
 package be.kuleuven.cs.distrinet.jnome.core.type;
 
+import org.aikodi.chameleon.core.lookup.LookupException;
 import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.oo.type.TypeFixer;
 import org.aikodi.chameleon.oo.type.generics.FormalParameterType;
 import org.aikodi.chameleon.oo.type.generics.FormalTypeParameter;
 import org.aikodi.chameleon.oo.type.generics.LazyFormalAlias;
@@ -19,6 +21,18 @@ public class JavaLazyFormalAlias extends LazyFormalAlias implements JavaType {
   @Override
   public FormalParameterType cloneSelf() {
     return new JavaLazyFormalAlias(name(), parameter());
+  }
+  
+  @Override
+  public boolean upperBoundNotHigherThan(Type other, TypeFixer trace) throws LookupException {
+    if(trace.contains(other, parameter())) {
+      return true;
+    } 
+    if(sameAs(other)) {
+      return true;
+    }
+    trace.add(other, parameter());
+    return super.upperBoundNotHigherThan(other, trace);
   }
 
 }

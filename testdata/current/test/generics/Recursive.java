71f423f23cd460f4ad65c92a72e0dadcf7026356
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-30 10:48:21 +0200

    updated current test case

--- testdata/current/test/generics/Recursive.java
+++ testdata/current/test/generics/Recursive.java
@@ -1,12 +1,12 @@
 package test.generics;
 
-public class Recursive<P extends Recursive<P>> {
+public class Recursive<X,P extends Recursive<X,P>> {
 
   public void x(P p) {
     m(p);
   }
 
-  public void m(Recursive<?> p) {
+  public void m(Recursive<X,?> p) {
 
   }
 

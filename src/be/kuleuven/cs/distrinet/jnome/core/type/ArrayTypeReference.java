44578f891dcd3608cb5f0ea578d77f0f665c525c
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-30 10:49:53 +0200

    Use this constructor delegation

--- src/be/kuleuven/cs/distrinet/jnome/core/type/ArrayTypeReference.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/ArrayTypeReference.java
@@ -33,8 +33,8 @@ public class ArrayTypeReference  extends ElementImpl implements JavaTypeReferenc
 		}
 	}
 	
-	public ArrayTypeReference(JavaTypeReference componentType) {
-		set(_elementType,componentType);
+	public ArrayTypeReference(JavaTypeReference elementType) {
+		this(elementType,1);
 	}
 	
   public JavaTypeReference elementTypeReference() {

31212684b7ea1edf177ef93ce2213e25f71a0729
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-30 10:47:59 +0200

    added missing cloneSelf

--- src/be/kuleuven/cs/distrinet/jnome/core/type/PureWildCardType.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/PureWildCardType.java
@@ -1,19 +1,30 @@
 package be.kuleuven.cs.distrinet.jnome.core.type;
 
+import org.aikodi.chameleon.core.element.Element;
 import org.aikodi.chameleon.core.lookup.LookupException;
 import org.aikodi.chameleon.oo.language.ObjectOrientedLanguage;
 import org.aikodi.chameleon.oo.type.Type;
-import org.aikodi.chameleon.oo.type.generics.WildCardType;
+import org.aikodi.chameleon.oo.type.generics.IntervalType;
 
-public class PureWildCardType extends WildCardType {
+public class PureWildCardType extends IntervalType {
 
 	public PureWildCardType(Type type) throws LookupException {
 		super("?",type ,type.language(ObjectOrientedLanguage.class).getNullType(type.view().namespace()));
 	}
 
-	@Override
+	protected PureWildCardType(String name, Type lowerBound, Type upperBound) {
+    super(name, lowerBound,upperBound);
+  }
+
+
+
+  @Override
 	public String getFullyQualifiedName() {
 		return "?";
 	}
 
+	@Override
+	protected Element cloneSelf() {
+	  return new PureWildCardType(name(),lowerBound(),upperBound());
+	}
 }

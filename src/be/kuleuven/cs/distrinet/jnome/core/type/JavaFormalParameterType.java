98239674c65a5363db84cbd766d433c6effbc95b
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-05-31 17:46:49 +0200

    Unroll

--- src/be/kuleuven/cs/distrinet/jnome/core/type/JavaFormalParameterType.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/JavaFormalParameterType.java
@@ -1,6 +1,8 @@
 package be.kuleuven.cs.distrinet.jnome.core.type;
 
+import org.aikodi.chameleon.core.lookup.LookupException;
 import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.oo.type.TypeFixer;
 import org.aikodi.chameleon.oo.type.generics.FormalParameterType;
 import org.aikodi.chameleon.oo.type.generics.FormalTypeParameter;
 
@@ -19,5 +21,15 @@ public class JavaFormalParameterType extends FormalParameterType implements Java
   public FormalParameterType cloneSelf() {
     return new JavaFormalParameterType(name(), aliasedType(), parameter());
   }
+
+  @Override
+  public boolean lowerBoundAtLeastAsHighAs(Type other, TypeFixer trace) throws LookupException {
+    if(trace.contains(other, parameter())) {
+      return true;
+    }
+    trace.add(other, parameter());
+    return other.upperBoundNotHigherThan(aliasedType(), trace);
+  }
+
 
 }

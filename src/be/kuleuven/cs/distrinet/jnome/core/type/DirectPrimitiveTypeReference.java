3792a84163a961c088e31bd7d11055651a179781
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-06-03 01:09:44 +0200

    added missing files

---
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/DirectPrimitiveTypeReference.java
@@ -0,0 +1,25 @@
+package be.kuleuven.cs.distrinet.jnome.core.type;
+
+import org.aikodi.chameleon.oo.type.Type;
+
+public class DirectPrimitiveTypeReference extends DirectJavaTypeReference {
+
+	public DirectPrimitiveTypeReference(Type type) {
+		super(type);
+	}
+	
+	@Override
+	protected DirectPrimitiveTypeReference cloneSelf() {
+		return new DirectPrimitiveTypeReference(getElement());
+	}
+	
+	@Override
+	public DirectPrimitiveTypeReference erasedReference() {
+		return cloneSelf();
+	}
+	
+	public JavaTypeReference componentTypeReference() {
+		return this;
+	}
+
+}
---
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/JavaUnionType.java
@@ -0,0 +1,43 @@
+package be.kuleuven.cs.distrinet.jnome.core.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aikodi.chameleon.core.lookup.LookupException;
+import org.aikodi.chameleon.exception.ChameleonProgrammerException;
+import org.aikodi.chameleon.oo.plugin.ObjectOrientedFactory;
+import org.aikodi.chameleon.oo.type.Type;
+import org.aikodi.chameleon.oo.type.UnionType;
+
+public class JavaUnionType extends UnionType implements JavaType {
+
+	public JavaUnionType(List<Type> types) {
+		super(types);
+	}
+
+	protected JavaUnionType(String name, List<Type> types) {
+		super(name, types);
+	}
+
+	@Override
+	protected UnionType cloneSelf() {
+		List<Type> types = types();
+		return new JavaUnionType(createSignature(types),types);
+	}
+	
+	@Override
+	public Type erasure() {
+	  throw new ChameleonProgrammerException();
+	}
+	
+	@Override
+	public Type captureConversion() throws LookupException {
+		List<Type> types = types();
+		List<Type> captured = new ArrayList<>(types.size());
+		for(Type type: types) {
+			captured.add(((JavaType)type).captureConversion());
+		}
+		return language().plugin(ObjectOrientedFactory.class).createUnionType(captured);
+	}
+
+}

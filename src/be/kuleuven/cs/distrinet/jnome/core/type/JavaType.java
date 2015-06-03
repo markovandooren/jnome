83f771cf311406628ed6c8bb3a529991f2f558ea
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-06-03 23:34:06 +0200

    use capture conversion on the correct side of the subtype relation

--- src/be/kuleuven/cs/distrinet/jnome/core/type/JavaType.java
+++ src/be/kuleuven/cs/distrinet/jnome/core/type/JavaType.java
@@ -25,17 +25,32 @@ public interface JavaType extends Type {
 	public default Type captureConversion() throws LookupException {
 		return this;
 	}
-	
 	@Override
 	public default boolean uniSubtypeOf(Type other, TypeFixer trace) throws LookupException {
-		 boolean result = false;
-		 if(other instanceof RawType) {
-			  result = superTypeJudge().get(other) != null;
-			} else if(other instanceof JavaType) {
-				Type snd = ((JavaType)other).captureConversion();
-				Type sameBase = getSuperType(snd);
-				result = sameBase != null && (sameBase instanceof RawType || sameBase.compatibleParameters(snd, trace));
-			}
-			return result;
+		Type captureConversion = captureConversion();
+		boolean result;
+		if(captureConversion != this) {
+			result = captureConversion.subtypeOf(other, trace);
+		} else {
+			result = Type.super.uniSubtypeOf(other, trace);
+		}
+		return result;
+//		Type snd = ((JavaType)other).captureConversion();
+//		Type sameBase = getSuperType(snd);
+//		boolean result = sameBase != null && sameBase.compatibleParameters(snd, trace);
+//		return result;
 	}
+	
+//	@Override
+//	public default boolean uniSubtypeOf(Type other, TypeFixer trace) throws LookupException {
+//		 boolean result = false;
+//		 if(other instanceof RawType) {
+//			  result = superTypeJudge().get(other) != null;
+//			} else if(other instanceof JavaType) {
+//				Type snd = ((JavaType)other).captureConversion();
+//				Type sameBase = getSuperType(snd);
+//				result = sameBase != null && (sameBase instanceof RawType || sameBase.compatibleParameters(snd, trace));
+//			}
+//			return result;
+//	}
 }

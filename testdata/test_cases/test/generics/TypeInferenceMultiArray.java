fd73a7d6066f1c45af904df6b23d7f087e27768b
Marko van Dooren <Marko.vanDooren@ugent.be>
2015-07-13 13:14:32 +0200

    added test case for inference of multi-dimensional arrays

---
+++ testdata/test_cases/test/generics/TypeInferenceMultiArray.java
@@ -0,0 +1,24 @@
+package test.generics;
+
+public class TypeInferenceMultiArray {
+
+	/**
+	 * Static because the original bug also used a static method.
+	 */
+	public static <T> T m(T[] t) {
+		return t;
+	}
+	
+	public void m() {
+		int[][] i = null;
+		// Correct: This calls <Integer> Integer m(Integer)
+		// False: before the fix, this resulted in <int> int m(int), which caused
+		//        the bounds check of the inferred type arguments to fail since int
+		//        is not a subtype of Object.
+		n(m(i));
+	}
+	
+	public void n(int[] i) {
+		
+	}
+}

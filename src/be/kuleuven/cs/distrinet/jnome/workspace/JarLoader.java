f0ec851040802f02cfd4de5bd24252603cff3c0d
Marko van Dooren <Marko.van.Dooren@gmail.com>
2016-06-07 01:20:40 +0200

    bugfix for inner classes with $ in the name. still not foolproof though

--- src/be/kuleuven/cs/distrinet/jnome/workspace/JarLoader.java
+++ src/be/kuleuven/cs/distrinet/jnome/workspace/JarLoader.java
@@ -1,9 +1,12 @@
 package be.kuleuven.cs.distrinet.jnome.workspace;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -22,7 +25,7 @@ import be.kuleuven.cs.distrinet.rejuse.predicate.Predicate;
 
 public class JarLoader extends AbstractZipScanner {
 
-	
+
 	/**
 	 * Create a new jar loader for the jar with the given path, file filter, and base loader setting.
 	 * 
@@ -30,7 +33,7 @@ public class JarLoader extends AbstractZipScanner {
 	 * @param filter A filter that selects files in the zip file based on their paths.
 	 * @param isBaseLoader Indicates whether the loader is responsible for loading a base library.
 	 */
- /*@
+	/*@
    @ public behavior
    @
    @ pre path != null;
@@ -48,7 +51,7 @@ public class JarLoader extends AbstractZipScanner {
 	 * @param path The path of the jar file from which elements must be loaded.
 	 * @param filter A filter that selects files in the zip file based on their paths.
 	 */
- /*@
+	/*@
    @ public behavior
    @
    @ pre path != null;
@@ -58,31 +61,35 @@ public class JarLoader extends AbstractZipScanner {
 	public JarLoader(JarFile path, Predicate<? super String,Nothing> filter) {
 		this(path, filter, false);
 	}
-	
+
 	@Override
 	protected void processMap(ZipFile jar, List<Pair<Pair<String, String>, ZipEntry>> names) throws InputException {
 		Map<String, ASMClassParser> map = new HashMap<String, ASMClassParser>();
-  	for(Pair<Pair<String,String>,ZipEntry> pair: names) {
-  		ZipEntry entry = pair.second();
-  		String qn = pair.first().second();
+		for(Pair<Pair<String,String>,ZipEntry> pair: names) {
+			ZipEntry entry = pair.second();
+			String qn = pair.first().second();
 			String name = Util.getLastPart(qn);
-			String packageFQN = namespaceFQN(entry.getName());
-			ASMClassParser parser = new ASMClassParser(jar,entry, name, packageFQN);
-			String second = Util.getAllButFirstPart(qn);
-			String key = (packageFQN == null ? name : packageFQN+"."+Util.getFirstPart(qn));
-			if(second != null) {
-				ASMClassParser asmClassParser = map.get(key);
-				// Deal with bad jars that contain class files of inner classes but not the outer class
-				// e.g. the OS X rt.jar.
-				if(asmClassParser != null) {
-					asmClassParser.add(parser, second);
+			if(name != null && ! name.isEmpty()) {
+				String packageFQN = namespaceFQN(entry.getName());
+				ASMClassParser parser = new ASMClassParser(jar,entry, name, packageFQN);
+				String second = Util.getAllButFirstPart(qn);
+				String key = (packageFQN == null ? name : packageFQN+"."+Util.getFirstPart(qn));
+				if(second != null) {
+					ASMClassParser asmClassParser = map.get(key);
+					// Deal with bad jars that contain class files of inner classes but not the outer class
+					// e.g. the OS X rt.jar.
+					if(asmClassParser != null) {
+						asmClassParser.add(parser, second);
+					}
+				} else {
+					map.put(key, parser);
+					DocumentLoaderNamespace ns = (DocumentLoaderNamespace) view().namespace().getOrCreateNamespace(packageFQN);
+					createInputSource(parser, ns);
 				}
 			} else {
-				map.put(key, parser);
-				DocumentLoaderNamespace ns = (DocumentLoaderNamespace) view().namespace().getOrCreateNamespace(packageFQN);
-				createInputSource(parser, ns);
+				Util.debug(true);
 			}
-  	}
+		}
 	}
 
 	protected void createInputSource(ASMClassParser parser, DocumentLoaderNamespace ns) throws InputException {
@@ -93,7 +100,7 @@ public class JarLoader extends AbstractZipScanner {
 	protected Language language() {
 		return view().language();
 	}
-	
+
 	@Override
 	public int compareTo(DocumentScanner o) {
 		//FIXME Just a hack for now, need proper support for a classpath.
@@ -104,9 +111,30 @@ public class JarLoader extends AbstractZipScanner {
 		}
 		return result;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Jar loader: "+file().getName();
 	}
+	
+	protected List<Pair<Pair<String, String>, ZipEntry>> createNameMap(ZipFile zip) {
+  	Enumeration<? extends ZipEntry> entries = zip.entries();
+		List<Pair<Pair<String,String>, ZipEntry>> names = new ArrayList<Pair<Pair<String,String>, ZipEntry>>();
+  	while(entries.hasMoreElements()) {
+  		ZipEntry entry = entries.nextElement();
+  		String name = entry.getName();
+  		if(filter().eval(name)) {
+  			String withoutSlashes = Util.getAllButLastPart(name).replace('/', '.');
+				String tmp = withoutSlashes.replace('$', '.');
+  			if(! tmp.matches(".*\\.[0-9].*")) {
+//  				String a = Util.getLastPart(withoutSlashes).replace('$', '.');
+  				String b = ASMClassParser.toDots(Util.getLastPart(withoutSlashes));
+//  				Util.debug(! a.equals(b));
+  				names.add(new Pair<Pair<String,String>, ZipEntry>(new Pair<String,String>(tmp,b), entry));
+  			}
+  		}
+  	}
+		return names;
+	}
+
 }

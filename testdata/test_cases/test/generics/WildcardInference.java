0fa9a57365ec297754ffa4c418f3674a6f161707
Marko van Dooren <Marko.van.Dooren@gmail.com>
2016-06-07 23:08:55 +0200

    Added test cases

--- testdata/test_cases/test/generics/WildcardInference.java
+++ testdata/test_cases/test/generics/WildcardInference.java
@@ -14,4 +14,33 @@ public class WildcardInference {
     m(v);
   }
 
+  public <T> void one(Collection<T> only) {
+  	
+  }
+  public <T> void two(Collection<T> first, Collection<T> second) {
+  	
+  }
+  
+  public void callK() {
+  	List<? extends Number> arg1 = null;
+  	List<?> arg2 = null;
+//  	two(arg1,arg2);
+  	one(arg1);
+  	one(arg2);
+  }
+  
+  public void numbers(Collection<Number> numbers) {
+  	
+  }
+  
+  public <T> T id(T t) {
+  	return t;
+  }
+  
+//  public void callNumbers() {
+//  	List<? extends Number> list = null;
+//  	List<? extends Number> id = id(list);
+//  	id.add(list.get(0));
+//  	numbers(id);
+//  }
 }
